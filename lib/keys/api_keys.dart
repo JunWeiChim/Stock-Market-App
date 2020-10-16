6cf423c0269ee4d12ef4e4c971ac28a7f3d1909c
JunWeiChim <junweichim1995@gmail.com>
2020-10-16 22:58:58 +0200

    Updated API keys

--- lib/keys/api_keys.dart
+++ lib/keys/api_keys.dart
@@ -1,30 +1,30 @@
 // TODO: You will need to get your own API keys in order to run this project.
 // TODO: Remove the 'package:sma/key.dart' package and replace the varibales' value with your own API keys.
 // The reason why the package in under is missing is because that's where I, (Joshua Garcia), store my API keys.
-// You do not need to create a new package key.dart because this is the one we will be using to store our keys. 
-import 'package:sma/key.dart';
+// You do not need to create a new package key.dart because this is the one we will be using to store our keys.
 
 /// Sentry DNS is used to track errors. It is beeing used at [SentryHelper].
-/// To get your DNS, go to: https://sentry.io/. 
+/// To get your DNS, go to: https://sentry.io/.
 /// Create a project and follow these steps: https://forum.sentry.io/t/where-can-i-find-my-dsn/4877
-const String kSentryDomainNameSystem = sentryDNS;
+const String kSentryDomainNameSystem =
+    'https://603f4c366a264c6197c2b12e9786d564@o462784.ingest.sentry.io/5466667';
 
 /// The Alpha Vantage API is used to power the Search Section.
-/// It is used at [SearchClient]. 
+/// It is used at [SearchClient].
 /// To get your own API key go to: https://www.alphavantage.co.
-const String kAlphaVantageKey = alphavantageKey;
+const String kAlphaVantageKey = 'XC0FU0BHOP0DSC4N';
 
 /// The Finnhub Stock API is used to power the news section in the [ProfileSection] page.
-/// It is used at [ProfileClient]. 
+/// It is used at [ProfileClient].
 /// To get your own API key go to: https://finnhub.io.
-const String kFinnhubKey = finnhubKey;
+const String kFinnhubKey = 'bu4taiv48v6sjdfq3ou0';
 
 /// The News API is used to power the news section.
-/// It is used at [NewsClient]. 
+/// It is used at [NewsClient].
 /// To get your own API key go to: https://newsapi.org.
-const String kNewsKey = newsApi;
+const String kNewsKey = 'c25ce7a5e5c2467fbd2182992cfc3d6e';
 
 /// Financial Modeling Prep API is used to power the Home, U.S Market and Profile Section.
 /// Now an API key is required which means that we won't be able to make infinite requests. :(
 /// To get your own API key go to: https://financialmodelingprep.com/developer/docs/
-const String kFinancialModelingPrepApi = financialAPI;
+const String kFinancialModelingPrepApi = 'cd0a25add111075dfdd0e25f3106445e';
