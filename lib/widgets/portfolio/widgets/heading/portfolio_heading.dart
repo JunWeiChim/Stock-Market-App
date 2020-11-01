66098d2b776eb95406652dd651359f926bb31e9d
JunWeiChim <junweichim1995@gmail.com>
2020-11-01 20:22:55 +0100

    fix day in date on home

--- lib/widgets/portfolio/widgets/heading/portfolio_heading.dart
+++ lib/widgets/portfolio/widgets/heading/portfolio_heading.dart
@@ -7,7 +7,7 @@ import 'package:sma/widgets/widgets/standard/header.dart';
 class PortfolioHeadingSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final String formattedDate = DateFormat('MMMM').format(DateTime.now());
+    final String formattedDate = DateFormat('MMMMd').format(DateTime.now());
 
     return StandardHeader(
       title: 'My portfolio',
